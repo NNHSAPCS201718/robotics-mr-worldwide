89c87be51a29ed59a176677f1e43c77fd3bb2381
TTaco73 <31216715+TTaco73@users.noreply.github.com>
2018-05-17 11:26:39 -0500

    worked on robots

---
+++ Hello World/Program1.java
@@ -0,0 +1,15 @@
+import lejos.nxt.*;
+/**
+ * Run Motor 1
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Program1
+{
+    public void main(String args[])
+    {
+       Motor.A.forward();
+    }
+    }
+
